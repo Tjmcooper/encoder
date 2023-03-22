ad31d7bbf10058a23398966523b271c10ca0f916
Ryleigh Sperry <ryleighsperry@Ryleighs-MacBook-Pro.local>
2023-03-22 13:08:04 -0400

    added

--- encoder.py
+++ encoder.py
@@ -6,17 +6,34 @@ def encode():
         encoded = (int(i) + 3) % 10
         code.append(encoded)
     code = ''.join(str(j) for j in code)
-    return
+    return code
+
+def decode(code):
+    new_password = ''
+    for i in code:
+        i = int(i)
+        i -= 3
+        i = str(i)
+        new_password += i
+    return new_password
+
+
+
 
 def main():
     while True:
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
         selection = input("Please enter an option: ")
         if selection == '1':
-            encode()
+            code = encode()
         elif selection == '2':
-            decode()
+            new_password = decode(code)
+            print(f"The encoded password is {code}, and the original password is {new_password}\n")
+
         elif selection == '3':
             break
         else:
             continue
+
+main()
+
